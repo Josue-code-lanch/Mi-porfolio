51af51296c8181ddf534ba418db26ba889d82286
Josue Gonzalez <josuegr834@gmail.com>
2024-07-22 12:56:36 -0300

    Eliminando el componente Skills

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,14 +3,12 @@ import ExperienceSection from "./ExperienceSection";
 import Proyectos from "./Proyectos";
 import About from "./About";
 import "../Styles/Main.css";
-import Skills from "./Skills";
 
 export default function Main() {
   return (
     <div className="container">
       <MainSection />
       <ExperienceSection />
-      <Skills />
       <Proyectos />
       <About />
     </div>
